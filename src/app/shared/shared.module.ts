2029b9bc3d323adaa08a950905bd777a0877eefa
nisargrbhatt <nisargrbhatt@gmail.com>
2022-02-23 19:32:09 +0530

    Prod error solved

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,41 +2,41 @@ import { RouterModule } from '@angular/router';
 import { AngularMaterialModule } from './../angular-material.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { SidenavComponent } from './sidenav/sidenav.component';
+// import { SidenavComponent } from './sidenav/sidenav.component';
 import { HeaderComponent } from './header/header.component';
 import { ErrorComponent } from './dialog/error/error.component';
 import { ResMesComponent } from './dialog/res-mes/res-mes.component';
 import { EmailVerificationComponent } from './dialog/email-verification/email-verification.component';
 import { LoaderComponent } from './loader/loader.component';
 import { ProductCardSmallComponent } from './product/product-card-small/product-card-small.component';
-import { ProductCardLongComponent } from './product/product-card-long/product-card-long.component';
+// import { ProductCardLongComponent } from './product/product-card-long/product-card-long.component';
 import { ProductImageUpdateComponent } from './product/product-image-update/product-image-update.component';
 import { BarLineChartComponent } from './charts/bar-line-chart/bar-line-chart.component';
 
 @NgModule({
   imports: [CommonModule, AngularMaterialModule, RouterModule],
   declarations: [
-    SidenavComponent,
+    // SidenavComponent,
     HeaderComponent,
     ErrorComponent,
     ResMesComponent,
     EmailVerificationComponent,
     LoaderComponent,
     ProductCardSmallComponent,
-    ProductCardLongComponent,
+    // ProductCardLongComponent,
     ProductImageUpdateComponent,
     BarLineChartComponent,
   ],
   providers: [],
   exports: [
-    SidenavComponent,
+    // SidenavComponent,
     HeaderComponent,
     ErrorComponent,
     ResMesComponent,
     EmailVerificationComponent,
     LoaderComponent,
     ProductCardSmallComponent,
-    ProductCardLongComponent,
+    // ProductCardLongComponent,
     ProductImageUpdateComponent,
     BarLineChartComponent,
   ],
